41dcb6f58781a9bb272e8a4b2f2c09342073f58a
ayoubbsn <bousnane45@gmail.com>
2023-01-23 21:05:31 +0100

    filter debut

--- Views/Client/Accueil.php
+++ Views/Client/Accueil.php
@@ -11,6 +11,13 @@ require_once __DIR__ . "/../../Controllers/AccueilController.php";
     <link rel='stylesheet' href='./css/Common.css'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <script src="./js/filter.js"></script>
+
+
+    <link rel="stylesheet" href="./css/FilterSort.css">
     <title>
         Accueil
     </title>
@@ -56,6 +63,8 @@ require_once __DIR__ . "/../../Controllers/AccueilController.php";
     </div>
     <br>
 
+
+
     <div style="text-align:center">
         <span class="dot"></span>
         <span class="dot"></span>
@@ -65,6 +74,62 @@ require_once __DIR__ . "/../../Controllers/AccueilController.php";
     <br>
     <br>
 
+    <div class="filterBy">
+        <div class="filter-option">
+            <label for="filter"> Filter by : </label>
+            <select name="filter" id="select-filter">
+                <option disabled selected value> -- select an option -- </option>
+                <option value="tmpprep"> Temps de preparation</option>
+                <option value="tmpcuiss"> Temps de cuisson</option>
+                <option value="tmptot"> Temps total</option>
+                <option value="notation"> Notation </option>
+                <option value="saison"> Saison </option>
+                <option value="calories"> Le nombre de calories</option>
+
+            </select>
+
+
+            <div class="fo tmpprep">
+                <label for="Temps de preparation"></label>
+                <input type="number" name="tmpprep" id="tmpprep">
+            </div>
+
+            <div class="fo tmpcuiss">
+                <input type="number" name="tmpcuiss" id="tmpcuiss">
+            </div>
+
+            <div class="fo tmptot">
+                <input type="number" name="tmptot" id="tmptot">
+
+            </div>
+
+            <div class="fo calories">
+                <input type="number" name="min" id="min">
+                <input type="number" name="max" id="max">
+            </div>
+
+            <div class="fo saison">
+                <select name="saisons" id="saisons">
+                    <option value="1"> Automne </option>
+                    <option value="2"> Hiver </option>
+                    <option value="3"> Printemps</option>
+                    <option value="4"> éte </option>
+                </select>
+            </div>
+
+            <div class="fo Notation">
+                <select name="saisons" id="saisons">
+                    <option value="1"> 1 Étoile </option>
+                    <option value="2"> 2 Étoiles </option>
+                    <option value="3"> 3 Étoiles </option>
+                    <option value="4"> 4 Étoiles </option>
+                    <option value="4"> 5 Étoiles </option>
+
+                </select>
+            </div>
+
+        </div>
+    </div>
 
     <div id="card-container">
 
@@ -121,7 +186,6 @@ require_once __DIR__ . "/../../Controllers/AccueilController.php";
 
 
     <script src="./js/Common.js"></script>
-
 </body>
 
 </html>
